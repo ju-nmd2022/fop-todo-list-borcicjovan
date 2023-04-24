f87aaac3200a3a430843945f72be2badd73e8254
borcicjovan <bojo22fp@student.ju.se>
2023-04-24 16:29:24 +0200

    To do list
    Added local storage that was missing

--- main.js
+++ main.js
@@ -6,12 +6,44 @@ document.addEventListener("DOMContentLoaded", function () {
   const addTaskInput = document.querySelector("#addtask input");
   const tasksList = document.querySelector("#tasks");
 
+  // function to save tasks to local storage, the thing i forgot in the first place...
+  function saveTasks() {
+    const tasks = tasksList.innerHTML;
+    localStorage.setItem("tasks", tasks);
+  }
+
+  // function to load tasks from local storage
+  function loadTasks() {
+    const savedTasks = localStorage.getItem("tasks");
+    if (savedTasks) {
+      tasksList.innerHTML = savedTasks;
+      // add event listeners to the buttons of loaded tasks
+      const doneButtons = document.querySelectorAll(".done");
+      doneButtons.forEach((button) => {
+        button.addEventListener("click", function () {
+          button.parentNode.remove();
+          saveTasks();
+        });
+      });
+      const deleteButtons = document.querySelectorAll(".delete");
+      deleteButtons.forEach((button) => {
+        button.addEventListener("click", function () {
+          button.parentNode.remove();
+          saveTasks();
+        });
+      });
+    }
+  }
+
+  // load saved tasks from local storage when you refresh the page
+  loadTasks();
+
   // event listener to the push button
   pushButton.addEventListener("click", function () {
     // Get the value of the new task input
     const taskName = addTaskInput.value.trim();
 
-    // alert message ofr empty task
+    // alert message for empty task
     if (taskName === "") {
       alert("No task name! Please enter one. 🥰");
       return;
@@ -24,22 +56,25 @@ document.addEventListener("DOMContentLoaded", function () {
       <button class="done">Done</button>
       <button class="delete">Delete</button>
     </div>
-  `;
+    `;
     tasksList.insertAdjacentHTML("beforeend", taskElement);
 
     // done button
     const doneButton = tasksList.lastElementChild.querySelector(".done");
     doneButton.addEventListener("click", function () {
-      tasksList.removeChild(this.parentNode);
+      doneButton.parentNode.remove();
+      saveTasks();
     });
 
     // delete button
     const deleteButton = tasksList.lastElementChild.querySelector(".delete");
     deleteButton.addEventListener("click", function () {
-      tasksList.removeChild(this.parentNode);
+      deleteButton.parentNode.remove();
+      saveTasks();
     });
 
     // Clear the new task input
     addTaskInput.value = "";
+    saveTasks();
   });
 });
